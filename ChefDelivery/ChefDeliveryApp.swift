1317147171838c385ae9f5292b23f639233b44be
Tamires Correa <tamirescorrea.dev@gmail.com>
2025-12-04 15:15:54 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Tamires Corrêa on 04/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
